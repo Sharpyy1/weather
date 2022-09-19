be9e26ce08f3af4bfd1e5fe0a8b1d5a4e82b64dd
Sharpyy <rpamazing11@gmail.com>
2022-09-19 16:46:45 -0500

    2nd

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
                   <div className='temp'>{Math.round(weather.main.temp)}°c</div>
                   <div className='weather'>{weather.weather[0].main}</div>
                 </div>
-              </div>):("Invalid input...")
+              </div>):(" ")
             }
           </div>
         </main>
